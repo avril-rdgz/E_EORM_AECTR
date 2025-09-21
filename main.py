62a2a77d77d313c5d10120395b70ac69b957a6bf
sofia-seo-j <sofia.e.jeong@gmail.com>
2025-09-21 16:49:11 +0200

    x range

--- main.py
+++ main.py
@@ -9,19 +9,19 @@ DATA_FILE = "crsp_data.csv"
 OUT_DIR = "outputs"
 os.makedirs(OUT_DIR, exist_ok=True)
 
-# --- Question 2 ---
+#---Question 2---#
 
 # Given parameters and initialization
 alpha = 0.4
 gamma = [0.01, 0.1, 1.0]
 delta = [0.3, 0.1, 0.0, -0.3]
 
-x = np.linspace(-1.25, 1.25,1000) # X-axis
-# x = np.linspace(-125, 125,1000) not visible
 
+# x = np.linspace(-1.25, 1.25,1000) # X-axis
+x = np.linspace(-5, 5,1000) 
 lines = ['-', '--', ':'] 
 
-# News Impact Curve definition followed by the given parameter setting as default
+# News Impact Curve definition followed by the model without constants, omega and beta.
 def nic(x, delta, gamma, mu = 0, lam = 0, sig2_init = 1, omega = 0, beta = 0):
     NIC = omega + (alpha + delta * np.tanh(-gamma * x)) * ((x-mu-lam*sig2_init)**2/(sig2_init)) + beta * sig2_init
     return NIC
@@ -32,10 +32,10 @@ axes = axes.ravel()
 
 for ax, d in zip(axes, delta):
     for g, style in zip(gamma, lines):
-        news_impact = nic(x, d, g)
-        ax.plot(x, news_impact, linestyle=style, label=fr"$\gamma={g}$")
+        sig = nic(x, d, g)
+        ax.plot(x, sig, linestyle=style, label=fr"$\gamma={g}$")
     ax.axvline(0, lw=0.3, alpha=0.7, color='grey')
-    ax.set_xticks(np.linspace(-1, 1, 5))  
+    ax.set_xticks(np.linspace(-5, 5, 5))  
     ax.set_title(fr"$\delta={d}$")
     ax.set_xlabel(r"Past shocks, $x_{t-1}$")
     ax.set_ylabel("News impact, $\sigma^2_{t}$")
